37149308264334c823a0b57b8cb67a2b747863f9
Daniel Hutmacher <daniel@strd.co>
2021-01-28 10:13:21 +0100

    Fixed duplicate CHECK_POLICY clauses
    Whenever the password changes, the script temporarily sets CHECK_POLICY=OFF for the duration password change. If CHECK_POLICY has also changed, this would generate an invalid SQL syntax.
    
    Thanks, Peter Varley!

--- SyncLogins.sql
+++ SyncLogins.sql
@@ -230,7 +230,8 @@ SELECT N'
 		(CASE WHEN x.[name]!=sp.[name]
 		      THEN ', NAME=['+x.[name]+N']'
 			  ELSE N'' END)+
-		(CASE WHEN x.is_policy_checked!=l.is_policy_checked
+		-- If CHECK_POLICY has changed, but the password hasn't:
+		(CASE WHEN x.password_hash=l.password_hash x.is_policy_checked!=l.is_policy_checked
 		      THEN ', CHECK_POLICY='+(CASE x.is_policy_checked WHEN 1 THEN N'ON' ELSE N'OFF' END)
 			  ELSE N'' END)+
 		(CASE WHEN x.is_expiration_checked!=l.is_expiration_checked
